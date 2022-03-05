8ee25604b94d6f9f45ab8a4747c0d1b897e7a1b5
cornpip <chdnjf1375@naver.com>
2022-03-05 23:44:21 +0900

    디렉토리 수정

--- baekjoon/test.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-const input = fs.readFileSync('./input.txt').toString().split(' ');
-const a = Number(input[0]);
-const b = Number(input[1]);
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
