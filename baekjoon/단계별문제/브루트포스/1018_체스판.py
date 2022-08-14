bcec1dbac6b0805ffbab25aa30fe0c464f6fad6c
cornpip <chdnjf1375@naver.com>
2022-08-15 02:23:35 +0900

    study : quick_sort

--- baekjoon/단계별문제/브루트포스/1018_체스판.py
+++ baekjoon/단계별문제/브루트포스/1018_체스판.py
@@ -19,28 +19,35 @@ def check(rows_cut):
                 if row_cut[i] != now:
                     count += 1
                 now = reverse(now)
-    count_1 = count
-    if count_1 == 0: return 0
-
-    count, flag = 0, "B"
-    for idx ,row_cut in enumerate(rows_cut):
-        if idx != 0:
-            flag = reverse(flag)
-        for i in range(8):
-            if i == 0:
-                if row_cut[i] != flag:
-                    count += 1
-                now = reverse(flag)
-            else:
-                if row_cut[i] != now:
-                    count += 1
-                now = reverse(now)
-    count_2 = count
-
-    if count_1 < count_2:
-        return count_1
+    
+    if 64-count > 32:
+        return count
     else:
-        return count_2
+        return 64-count
+    # 뒤집고 돌려볼 필요없다.
+    
+    # count_1 = count
+    # if count_1 == 0: return 0
+
+    # count, flag = 0, "B"
+    # for idx ,row_cut in enumerate(rows_cut):
+    #     if idx != 0:
+    #         flag = reverse(flag)
+    #     for i in range(8):
+    #         if i == 0:
+    #             if row_cut[i] != flag:
+    #                 count += 1
+    #             now = reverse(flag)
+    #         else:
+    #             if row_cut[i] != now:
+    #                 count += 1
+    #             now = reverse(now)
+    # count_2 = count
+
+    # if count_1 < count_2:
+    #     return count_1
+    # else:
+    #     return count_2
 
 n, m = map(int, input().split())
 
--- baekjoon/단계별문제/정렬/quick_sort.py
+++ baekjoon/단계별문제/정렬/quick_sort.py
@@ -1,22 +1,39 @@
+import sys
+sys.setrecursionlimit(10000)
+
 def quick_sort(arr, start, end):
     if start >= end:
         return
     pivot = start
     left = start + 1
     right = end
-    while left <= right:
+    while 1:
         while arr[pivot] >= arr[left] and left< end:
             left += 1
         while arr[pivot] <= arr[right] and right > start:
             right -= 1
         if left >= right:
             arr[pivot], arr[right] = arr[right], arr[pivot]
+            # print("break", arr)
+            break
         else:
             arr[left], arr[right] = arr[right], arr[left]
+        print(arr, left, right)
     quick_sort(arr, start, right-1)
     quick_sort(arr, right+1, end)
-    # return arr
 
-arr = [15,15,15]
-a = quick_sort(arr, 0, len(arr)-1) 
-print(arr)           
+# n = int(input())
+# arr = []
+# for i in range(n):
+#     arr.append(int(input()))
+
+arr = [ 2, 6, 7, 5, 9, 0, 1, 4, 3, 8]
+quick_sort(arr, 0, len(arr)-1) 
+# for i in arr:
+#     print(i)
+print(arr)
+
+# 그냥 정렬 문제
+# sort: 104ms, quick_sort: 160ms, 메모리는 비슷
+# 백준 최대 재귀 깊이는 1,000 으로 제한되어 있다고함
+# 재귀썼는데 RecursionError 나오면 고려
