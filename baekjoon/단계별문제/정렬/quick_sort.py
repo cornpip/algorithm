f8d9bcf07c05874e495305491908b95ad6dc1ab4
cornpip <chdnjf1375@naver.com>
2022-08-30 23:40:38 +0900

    chore : 노트북 이동

--- baekjoon/단계별문제/정렬/quick_sort.py
+++ baekjoon/단계별문제/정렬/quick_sort.py
@@ -22,18 +22,14 @@ def quick_sort(arr, start, end):
     quick_sort(arr, start, right-1)
     quick_sort(arr, right+1, end)
 
-# n = int(input())
-# arr = []
-# for i in range(n):
-#     arr.append(int(input()))
-
 arr = [ 2, 6, 7, 5, 9, 0, 1, 4, 3, 8]
 quick_sort(arr, 0, len(arr)-1) 
-# for i in arr:
-#     print(i)
 print(arr)
 
 # 그냥 정렬 문제
 # sort: 104ms, quick_sort: 160ms, 메모리는 비슷
 # 백준 최대 재귀 깊이는 1,000 으로 제한되어 있다고함
 # 재귀썼는데 RecursionError 나오면 고려
+
+# +) quick_sort: arr잘라서 넘겨주고 return붙이는 식으로 가면 start, end 안가져갈 수도 있다.
+# +) 아이디어 보다 구현에서 부등호가 헷갈린다
